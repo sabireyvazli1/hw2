121e0db0b87145e0582bc5f64787348cf4f64852
sabireyvazli1 <sabireyvazli@gmail.com>
2023-09-05 15:24:38 +0400

    first

---
+++ app.js
@@ -0,0 +1,128 @@
+let database =[
+    {
+
+    }
+]
+const nameINP = document.querySelector(".nameINP");
+const soyadINP = document.querySelector(".soyadINP");
+const ageINP = document.querySelector(".ageINP");
+const emailINP = document.querySelector(".emailINP");
+const passwordINP = document.querySelector(".passwordINP");
+const genderINP = document.querySelectorAll(".genderINP");
+const hobbyINP = document.querySelectorAll(".hobbyINP");
+const resetBTN = document.querySelector(".resetBTN");
+
+const forrm = document.querySelector(".forrm");
+forrm.addEventListener("submit",function(e){
+    e.preventDefault();
+    if (nameINP.value.length > 2 && soyadINP.value.length >2 && ageINP.value > 18) {
+        validation(nameINP.value, soyadINP.value, ageINP.value, emailINP.value, passwordINP.value);
+        }
+        else {
+        alert("ugursuzdur");
+        nameINP.value = "";
+        soyadINP.value = "";
+        ageINP.value = "";
+        emailINP.value ="";
+        passwordINP.value ="";
+        genderINP.forEach((radio) => {
+            radio.checked = false;
+        });
+        hobbyINP.forEach((checkbox) => {
+            checkbox.checked = false;
+        });
+        }
+
+    function validation(a, s, d, h, j) {
+        let salam;
+        let obj = {};
+        obj.name = a;
+        obj.surname = s;
+        obj.age = d;
+        const selectedHobbies = [];
+        hobbyINP.forEach((checkbox) => {
+            if (checkbox.checked) {
+             selectedHobbies.push(checkbox.value);
+            }
+        });
+        obj.hobbies = selectedHobbies;
+
+        genderINP.forEach((radio) => {
+            if (radio.checked){
+                obj.gender = radio.value;
+            }
+        })
+        obj.email = h;
+        obj.password = j;
+        database.push(obj);
+        console.log(database);
+        nameINP.value = "";
+        soyadINP.value = "";
+        ageINP.value = "";
+        emailINP.value ="";
+        passwordINP.value ="";
+        genderINP.forEach((radio) => {
+            radio.checked = false;
+        });
+        hobbyINP.forEach((checkbox) => {
+            checkbox.checked = false;
+        });
+    }
+    resetBTN.addEventListener("click", function () {
+        nameINP.value = "";
+        soyadINP.value = "";
+        ageINP.value = "";
+        emailINP.value = "";
+        passwordINP.value = "";
+    
+        // Uncheck all gender radio buttons
+        genderINP.forEach((radio) => {
+            radio.checked = false;
+        });
+    
+        // Uncheck all hobby checkboxes
+        hobbyINP.forEach((checkbox) => {
+            checkbox.checked = false;
+        });
+    });
+})   
+//print elements of database
+function print_elements(){
+    for(let i; i < database.length ; i++){
+        console.log(i);
+    }
+}
+
+//print names from database
+function print_names() {
+   console.log(database.name)
+}
+
+//print names and surnames from database
+function print_names_surnames(){
+    console.log(database.name+" "+database.surname)
+}
+
+// Eyni zamanda iki ədəd arasında toplama, çıxma, vurma, 
+// bölmə əməliyyatlarını yerinə yetirən funsiya yazın. 
+// Funksiya dinamik olmalıdır yəni ədədləri parametr olaraq almalıdır.
+let bir;
+let iki;
+function emeliyyatrlar(bir, iki){
+    console.log("toplama= "+ (bir + iki))
+    console.log("cixma= "+ (bir - iki))
+    console.log("vurma= "+ (bir * iki))
+    console.log("toplama= "+ (bir / iki))
+}
+
+// Ədədin tək və ya cüt olduğunu təyin edən funksiya yazın. 
+// Funksiya dinamik olmalıdır yəni ədədi parametr olaraq almalıdır.
+function yoxlama(bir){
+    if(bir % 2 == 1){
+        console.log("tekdir")
+    }else if(bir == 0){
+        console.log("0 dir")
+    }else if(bir % 2 == 0){
+        colsole.log("cutdur")
+    }
+}
