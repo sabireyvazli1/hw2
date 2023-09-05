b8b3dbfe5b026459605ce8846341e88de13ef2b6
sabireyvazli1 <sabireyvazli@gmail.com>
2023-09-05 22:04:05 +0400

    second

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 let database =[
     {
-
+        age : "20",
+        email: "salam@gmail.com",
+        gender: "kisi",
+        hobbies: ['cricket', 'hokky', 'chess'],
+        name: "sabir",
+        password: "ksksksksksk",
+        surname: "eyvazli"
+    },
+    {
+        age : "99",
+        email: "jnjnjn@gmail.com",
+        gender: "kisi",
+        hobbies: ['cricket', 'hokky', 'chess'],
+        name: "rustem",
+        password: "ksksksksksk",
+        surname: "aliyev"
     }
 ]
 const nameINP = document.querySelector(".nameINP");
@@ -55,7 +70,7 @@ forrm.addEventListener("submit",function(e){
         obj.email = h;
         obj.password = j;
         database.push(obj);
-        console.log(database);
+        //console.log(database);
         nameINP.value = "";
         soyadINP.value = "";
         ageINP.value = "";
@@ -88,21 +103,27 @@ forrm.addEventListener("submit",function(e){
 })   
 //print elements of database
 function print_elements(){
-    for(let i; i < database.length ; i++){
-        console.log(i);
+    for(let i=0; i < database.length ; i++){
+        console.log(database[i]);
     }
 }
+print_elements();
 
 //print names from database
 function print_names() {
-   console.log(database.name)
+    for (let i = 0; i < database.length; i++) {
+        console.log(database[i].name);
+    }
 }
+print_names();
 
 //print names and surnames from database
 function print_names_surnames(){
-    console.log(database.name+" "+database.surname)
+    for(let i=0;i < database.length;i++){
+        console.log(database[i].name+" "+database[i].surname)
+    }
 }
-
+print_names_surnames();
 // Eyni zamanda iki ədəd arasında toplama, çıxma, vurma, 
 // bölmə əməliyyatlarını yerinə yetirən funsiya yazın. 
 // Funksiya dinamik olmalıdır yəni ədədləri parametr olaraq almalıdır.
@@ -114,15 +135,18 @@ function emeliyyatrlar(bir, iki){
     console.log("vurma= "+ (bir * iki))
     console.log("toplama= "+ (bir / iki))
 }
-
+emeliyyatrlar(2, 2)
 // Ədədin tək və ya cüt olduğunu təyin edən funksiya yazın. 
 // Funksiya dinamik olmalıdır yəni ədədi parametr olaraq almalıdır.
 function yoxlama(bir){
-    if(bir % 2 == 1){
-        console.log("tekdir")
+    if(bir % 2 == 0 && bir != 0){
+        console.log("cutdur");
     }else if(bir == 0){
-        console.log("0 dir")
-    }else if(bir % 2 == 0){
-        colsole.log("cutdur")
+        console.log("0 dir");
+    }else if(bir % 2 == 1){
+        console.log("tekdir");
     }
 }
+yoxlama(5)
+yoxlama(2)
+yoxlama(0)
